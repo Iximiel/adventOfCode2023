36bd879f036589c0ef04126c5a8ec6ea449cb232
Daniele Rapetti <iximiel@gmail.com>
2023-12-07 22:15:15 +0100

    did day 2 :)

--- aoc23_02/bin/main.ml
+++ aoc23_02/bin/main.ml
@@ -3,6 +3,7 @@ let isvalidgame = BagOcubes.isPossibleGame 12 13 14
 let () =
   let filename = "day2" in
   let mysum = ref 0 in
+  let mypow = ref 0 in
   let chan = open_in filename in
   try
     while true do
@@ -14,9 +15,14 @@ let () =
       let extractions = BagOcubes.getExtractions line in
       List.iter (BagOcubes.pp Format.std_formatter) extractions;
       mysum := !mysum + if isvalidgame line then gameId else 0;
+      mypow := !mypow + (BagOcubes.minimumBag extractions |> BagOcubes.gamePower);
       print_endline ""
     done
   with End_of_file ->
     close_in chan;
+    print_string "task 1: ";
     print_int !mysum;
+    print_endline "";
+    print_string "task 2: ";
+    print_int !mypow;
     print_endline ""
--- aoc23_02/test/test_aoc23_02.ml
+++ aoc23_02/test/test_aoc23_02.ml
@@ -23,6 +23,11 @@ let test_gameExtr s res () =
     "same extraction" res
     (BagOcubes.getExtractions s)
 
+let test_power s res () =
+  Alcotest.(check int)
+    "same number" res
+    (BagOcubes.getExtractions s |> BagOcubes.minimumBag |> BagOcubes.gamePower)
+
 (* Run it *)
 let () =
   let open Alcotest in
@@ -140,5 +145,39 @@ let () =
             (test_possibleExtraction10_10_10
                (BagOcubes.makeExtraction "5 green, 1 red")
                true);
+        ] );
+      ( "Games power",
+        [
+          test_case "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+            `Quick
+            (test_power "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+               48);
+          test_case
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+            `Quick
+            (test_power
+               "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 \
+                blue"
+               12);
+          test_case
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 \
+             green, 1 red"
+            `Quick
+            (test_power
+               "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 \
+                green, 1 red"
+               1560);
+          test_case
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, \
+             14 red"
+            `Quick
+            (test_power
+               "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 \
+                blue, 14 red"
+               630);
+          test_case "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+            `Quick
+            (test_power "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+               36);
         ] );
     ]
