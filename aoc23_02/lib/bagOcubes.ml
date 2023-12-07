cd9923e0f302270937112cd38be8b6040c8851d6
Daniele Rapetti <iximiel@gmail.com>
2023-12-07 17:32:58 +0100

    more or less it works

--- aoc23_02/lib/bagOcubes.ml
+++ aoc23_02/lib/bagOcubes.ml
@@ -18,3 +18,11 @@ let getRed = numFromString redRegex
 let getBlue = numFromString blueRegex
 let getGreen = numFromString greenRegex
 let makeExtraction s = { r = getRed s; g = getGreen s; b = getBlue s }
+let isPossibleGame r g b extr = r >= extr.r && g >= extr.g && b >= extr.b
+let gameRegex = Str.regexp {|Game \b\([0-9]+\)\b:|}
+let getGameID = numFromString gameRegex
+
+let getExtractions gameString =
+  let gameAndExtr = String.split_on_char ':' gameString in
+  let extractions = String.split_on_char ';' (List.nth gameAndExtr 1) in
+  List.map makeExtraction extractions
--- aoc23_02/test/test_aoc23_02.ml
+++ aoc23_02/test/test_aoc23_02.ml
@@ -1,6 +1,9 @@
 (* The tests *)
 let test_color getColor s res () =
-  Alcotest.(check int) "same string" res (getColor s)
+  Alcotest.(check int) "same result" res (getColor s)
+
+let test_game s res () =
+  Alcotest.(check int) "same result" res (BagOcubes.getGameID s)
 
 let test_red = test_color BagOcubes.getRed
 let test_green = test_color BagOcubes.getGreen
@@ -9,6 +12,11 @@ let extraction = Alcotest.testable BagOcubes.pp BagOcubes.equal
 
 let test_read s res () =
   Alcotest.(check extraction) "same extraction" res (BagOcubes.makeExtraction s)
+
+let test_gameExtr s res () =
+  Alcotest.(check (list extraction))
+    "same extraction" res
+    (BagOcubes.getExtractions s)
 
 (* Run it *)
 let () =
@@ -47,5 +55,62 @@ let () =
             (test_read "5 blue, 4 red, 13 green" { r = 4; g = 13; b = 5 });
           test_case "2 blue, 1 red, 2 green" `Quick
             (test_read "2 blue, 1 red, 2 green" { r = 1; g = 2; b = 2 });
+        ] );
+      ( "Games id",
+        [
+          test_case "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+            `Quick
+            (test_game "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+               1);
+          test_case
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+            `Quick
+            (test_game
+               "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 \
+                blue"
+               2);
+          test_case
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 \
+             green, 1 red"
+            `Quick
+            (test_game
+               "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 \
+                green, 1 red"
+               3);
+          test_case
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, \
+             14 red"
+            `Quick
+            (test_game
+               "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 \
+                blue, 14 red"
+               4);
+          test_case "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+            `Quick
+            (test_game "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+               5);
+        ] );
+      ( "test_gameExtr",
+        [
+          test_case "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+            `Quick
+            (test_gameExtr
+               "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+               [
+                 BagOcubes.makeExtraction "6 red, 1 blue, 3 green";
+                 BagOcubes.makeExtraction "2 blue, 1 red, 2 green";
+               ]);
+          test_case
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 \
+             green, 1 red"
+            `Quick
+            (test_gameExtr
+               "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 \
+                green, 1 red"
+               [
+                 BagOcubes.makeExtraction "8 green, 6 blue, 20 red";
+                 BagOcubes.makeExtraction "5 blue, 4 red, 13 green";
+                 BagOcubes.makeExtraction "5 green, 1 red";
+               ]);
         ] );
     ]
