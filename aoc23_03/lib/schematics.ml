688cbafbccd20ca634c833c12ccbdd6e156e2280
Daniele Rapetti <iximiel@gmail.com>
2023-12-08 02:11:25 +0100

    ready for solution

--- aoc23_03/lib/schematics.ml
+++ aoc23_03/lib/schematics.ml
@@ -1,16 +1,54 @@
-let numRegex = Str.regexp {|\b\([0-9]+\)\b|}
+let numRegex = Str.regexp {|\b[0-9]+\b|}
+let dotOrNUm = Str.regexp {|[\.0-9]|}
+
+let checkL line pos len =
+  if String.equal line "" then false
+  else
+    let substr = String.sub line (max 0 (pos - 1)) (len + 2) in
+    print_endline @@ line ^ "->\"" ^ substr ^ "\"";
+    print_endline @@ string_of_int
+    @@ String.length (Str.global_replace dotOrNUm "" substr);
+    String.length (Str.global_replace dotOrNUm "" substr) > 0
 
 let checkOnline s lb la =
-  let rec aux = function
+  let checkLB pos t d =
+    checkL lb
+      (pos + String.length t)
+      (String.length d - if pos == 0 then 1 else 0)
+  in
+  let checkLA pos t d =
+    checkL la
+      (pos + String.length t)
+      (String.length d - if pos == 0 then 1 else 0)
+  in
+  let checkLAB pos t d = checkLA pos t d || checkLB pos t d in
+  let rec aux reres pos =
+    match reres with
     | Str.Text t :: (Delim d :: l as ld) ->
-        if Str.last_chars t 1 != "." then int_of_string d :: aux l else aux ld
+        if not @@ String.ends_with ~suffix:"." t then
+          int_of_string d :: aux l (pos + String.length t + String.length d)
+        else if checkLAB pos t d then
+          int_of_string d :: aux l (pos + String.length t + String.length d)
+        else aux ld (pos + String.length t)
     | Str.Delim d :: (Text t :: _ as l) ->
-        if Str.first_chars t 1 != "." then int_of_string d :: aux l else aux l
+        if not @@ String.starts_with ~prefix:"." t then
+          int_of_string d :: aux l (pos + String.length d)
+        else if checkLAB pos "" d then
+          int_of_string d :: aux l (pos + String.length d)
+        else aux l (pos + String.length d)
     | Delim _ :: [] -> []
     | Text _ :: [] -> []
     | [] -> []
     (* impossible matches *)
-    | Delim _ :: Delim _ :: _ -> [ int_of_string lb ]
-    | Text _ :: Text _ :: _ -> [ int_of_string la ]
+    | Delim _ :: Delim _ :: _ -> []
+    | Text _ :: Text _ :: _ -> []
   in
-  Str.full_split numRegex s |> aux
+  aux (Str.full_split numRegex s) 0
+
+let rec checkWholeSchema schema =
+  match schema with
+  | lb :: (l :: la :: _ as nextLine) ->
+      List.append (checkOnline l lb la) (checkWholeSchema nextLine)
+  | [ lb; l ] -> checkOnline l lb ""
+  | [ l ] -> checkOnline l "" ""
+  | [] -> []
--- aoc23_03/test/test_aoc23_03.ml
+++ aoc23_03/test/test_aoc23_03.ml
@@ -12,12 +12,20 @@ let example =
     ".664.598..";
   ]
 
+let exampleres = [ 467; 35; 633; 617; 592; 755; 664; 598 ]
 let exampleLine = List.nth example
 
-let test_numberlist_sl s res () =
+let test_full mylist res () =
   Alcotest.(check (list int))
-    "same extraction" res
-    (Schematics.checkOnline s "" "")
+    "same list" res
+    (Schematics.checkWholeSchema ("" :: mylist))
+
+let test_numberlist s lb la res () =
+  Alcotest.(check (list int)) "same list" res (Schematics.checkOnline s lb la)
+
+let test_numberlist_sl s res () = test_numberlist s "" "" res ()
+let test_numberlist_lb s lb res () = test_numberlist s lb "" res ()
+let test_numberlist_la s la res () = test_numberlist s "" la res ()
 
 let () =
   let open Alcotest in
@@ -27,7 +35,35 @@ let () =
         [
           test_case "single match" `Quick
             (test_numberlist_sl (exampleLine 4) [ 617 ]);
+          test_case "no match" `Quick (test_numberlist_sl (exampleLine 2) []);
+          test_case "no match" `Quick (test_numberlist_sl ".5." []);
+          test_case "single match" `Quick (test_numberlist_sl "*617" [ 617 ]);
+          test_case "single match" `Quick
+            (test_numberlist_sl ".3*617" [ 3; 617 ]);
           test_case "double match" `Quick
             (test_numberlist_sl "..617*...]89..." [ 617; 89 ]);
         ] );
+      ( "line before",
+        [
+          test_case "no match" `Quick (test_numberlist_lb ".35..." "....*." []);
+          test_case "no match" `Quick (test_numberlist_lb "...35." ".*...." []);
+          test_case "single match" `Quick
+            (test_numberlist_lb "35..." "..*.." [ 35 ]);
+          test_case "single match" `Quick
+            (test_numberlist_lb (exampleLine 2) "123*5678901" [ 35 ]);
+          test_case "single match" `Quick
+            (test_numberlist_lb (exampleLine 2) (exampleLine 1) [ 35 ]);
+        ] );
+      ( "line after",
+        [
+          test_case "no match" `Quick (test_numberlist_la ".35..." "....*." []);
+          test_case "no match" `Quick (test_numberlist_la "...35." ".*...." []);
+          test_case "single match" `Quick
+            (test_numberlist_la "35..." "..*.." [ 35 ]);
+          test_case "single match" `Quick
+            (test_numberlist_la (exampleLine 2) "123*5678901" [ 35 ]);
+          test_case "single match" `Quick
+            (test_numberlist_la (exampleLine 2) (exampleLine 3) [ 633 ]);
+        ] );
+      ("example", [ test_case "full run" `Quick (test_full example exampleres) ]);
     ]
