f67565b4ba6a81561378a749ce422f215e8e8bc2
Daniele <Iximiel@users.noreply.github.com>
2023-12-15 01:19:23 +0100

    i had inverted the source and the destination

--- aoc23/lib/gardener.ml
+++ aoc23/lib/gardener.ml
@@ -16,7 +16,7 @@ let ppft ppf { source; dest } =
    a negative integer if x is less than y,
     and a positive integer if x is greater than y.
     This ASSUMES that all the intervals here are not overlapped*)
-let compare (a : fromTo) (b : fromTo) = a.source.low - b.source.low
+let compareft (a : fromTo) (b : fromTo) = a.source.low - b.source.low
 (* if a.low < b.low then -1 else if a.low > b.low then 1 else 0 *)
 
 let isIn intv i = intv.low <= i && i <= intv.up
@@ -25,17 +25,74 @@ let getPos intv i =
   if i < intv.low then UNDER else if intv.up < i then ABOVE else IN
 
 let getShift intv i = i - intv.low
-let mapTo intvTo intvFrom i = intvTo.low + getShift intvFrom i
+let mapTo intvFrom intvTo i = intvTo.low + getShift intvFrom i
 
 let strToFromTo s =
   (*strings are sourcelow destLow interval*)
   let notEmpty str = String.equal String.empty str |> not in
   let splitted = String.split_on_char ' ' s |> List.filter notEmpty in
   let get = List.nth splitted in
-  let sl = get 0 |> int_of_string in
-  let dl = get 1 |> int_of_string in
-  let myint = get 2 |> int_of_string in
+  let sl = get 1 |> int_of_string in
+  let dl = get 0 |> int_of_string in
+  (* the interval is of myint, without the -1 i will get one more number in*)
+  let myint = (get 2 |> int_of_string) - 1 in
   {
     source = { low = sl; up = sl + myint };
     dest = { low = dl; up = dl + myint };
   }
+
+let getMap ls = List.map strToFromTo ls |> List.sort compareft
+
+let convert map i =
+  let rec aux m =
+    match m with
+    | [] -> i
+    | intv :: lm ->
+        if isIn intv.source i then mapTo intv.source intv.dest i else aux lm
+  in
+  aux map
+
+(* reader *)
+type completeMap = {
+  seeds : int list;
+  se2so : fromTo list;
+  so2f : fromTo list;
+  f2w : fromTo list;
+  w2li : fromTo list;
+  li2t : fromTo list;
+  t2h : fromTo list;
+  h2lo : fromTo list;
+}
+
+let reader ls =
+  let rec aux l r =
+    match l with
+    | [] -> [ List.rev r ]
+    | "" :: ll -> List.rev r :: aux ll []
+    | any :: ll -> aux ll (any :: r)
+  in
+  let mm = aux (List.tl ls) [] in
+  let notEmpty str = String.equal String.empty str |> not in
+  let tmp =
+    String.split_on_char ' ' (List.hd ls)
+    |> List.filter notEmpty |> List.tl |> List.map int_of_string
+  in
+  {
+    seeds = tmp;
+    (*skips the first blank line*)
+    se2so = getMap @@ List.tl @@ List.nth mm 1;
+    so2f = getMap @@ List.tl @@ List.nth mm 2;
+    f2w = getMap @@ List.tl @@ List.nth mm 3;
+    w2li = getMap @@ List.tl @@ List.nth mm 4;
+    li2t = getMap @@ List.tl @@ List.nth mm 5;
+    t2h = getMap @@ List.tl @@ List.nth mm 6;
+    h2lo = getMap @@ List.tl @@ List.nth mm 7;
+  }
+
+let getLocations myMap =
+  let translator i =
+    convert myMap.se2so i |> convert myMap.so2f |> convert myMap.f2w
+    |> convert myMap.w2li |> convert myMap.li2t |> convert myMap.t2h
+    |> convert myMap.h2lo
+  in
+  List.map translator myMap.seeds
--- aoc23/test/test_aoc23_05.ml
+++ aoc23/test/test_aoc23_05.ml
@@ -35,8 +35,6 @@ let example =
     "56 93 4";
   ]
 
-let _ = example
-
 (* let interval = Alcotest.testable Gardener.ppint Gardener.equalint *)
 let fromTo = Alcotest.testable Gardener.ppft Gardener.equalft
 
@@ -49,6 +47,25 @@ let test_isNotIn myint num () =
 let test_parseFT s res () =
   Alcotest.(check fromTo) "same result" res (Gardener.strToFromTo s)
 
+let test_parseFTM ls res () =
+  Alcotest.(check @@ list fromTo) "same result" res (Gardener.getMap ls)
+
+let test_convert ls i res () =
+  Alcotest.(check int)
+    "same result" res
+    (Gardener.convert (Gardener.getMap ls) i)
+
+let ttt () =
+  Alcotest.(check @@ list fromTo)
+    "same result"
+    (Gardener.getMap [ "45 77 23"; "81 45 19"; "68 64 13" ])
+    (Gardener.reader example).li2t
+
+let test_game () =
+  Alcotest.(check @@ list int)
+    "same result" [ 35 ]
+    (Gardener.getLocations (Gardener.reader example) |> List.sort ( - ))
+
 let () =
   let open Alcotest in
   run "Utils"
@@ -59,10 +76,39 @@ let () =
           test_case "is in" `Quick (test_isIn { low = 1; up = 3 } 1);
           test_case "is not in" `Quick (test_isNotIn { low = 1; up = 3 } 4);
         ] );
-      ( "FromTp",
+      ( "FromTo",
         [
           test_case "parser" `Quick
             (test_parseFT "80 30 5"
-               { source = { low = 80; up = 85 }; dest = { low = 30; up = 35 } });
+               { source = { low = 30; up = 34 }; dest = { low = 80; up = 84 } });
+          test_case "parser" `Quick
+            (test_parseFT "80 30 1"
+               { source = { low = 30; up = 30 }; dest = { low = 80; up = 80 } });
+          test_case "parser" `Quick
+            (test_parseFT "45 79 5"
+               { source = { low = 79; up = 83 }; dest = { low = 45; up = 49 } });
+          test_case "parser list" `Quick
+            (test_parseFTM [ "30 80 5"; "79 45 5" ]
+               [
+                 {
+                   source = { low = 45; up = 49 };
+                   dest = { low = 79; up = 83 };
+                 };
+                 {
+                   source = { low = 80; up = 84 };
+                   dest = { low = 30; up = 34 };
+                 };
+               ]);
+          test_case "converter" `Quick
+            (test_convert [ "30 80 5"; "79 45 5" ] 10 10);
+          test_case "converter" `Quick
+            (test_convert [ "30 80 5"; "79 45 5" ] 81 31);
+          test_case "converter" `Quick
+            (test_convert [ "30 80 5"; "79 45 5" ] 46 80);
+          test_case "converter" `Quick
+            (test_convert [ "50 98 2"; "52 50 48" ] 79 81);
         ] );
+      ( "reader",
+        [ test_case "reader" `Quick ttt; test_case "reader" `Quick test_game ]
+      );
     ]
